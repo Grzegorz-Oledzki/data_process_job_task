15614da60bcbb1557e0e2cbd35afba40997669c3
Grzegorz-Oledzki <grzeoledzki@gmail.com>
2024-10-30 23:27:21 +0100

    changes urls.py

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from core.views import IndexView
+from core.views import IndexView, DatasetView, DatasetListView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path("", IndexView.as_view(), name="index"),
+    path("download/", DatasetView.as_view(), name="dataset"),
+    path("datasets/", DatasetListView.as_view(), name="dataset_list"),
 ]
