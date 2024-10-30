16df2e0fb8909c13655d0be1610bc7cc73776b42
Grzegorz-Oledzki <grzeoledzki@gmail.com>
2024-10-30 23:27:13 +0100

    add model

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Dataset(models.Model):
+    filename = models.CharField(max_length=255)
+    download_date = models.DateTimeField(auto_now_add=True)
