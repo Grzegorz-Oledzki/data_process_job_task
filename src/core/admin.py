8dd2dbdd7c9c4b58ff3adb4b2cfa14ddfac3b4c4
Grzegorz-Oledzki <grzeoledzki@gmail.com>
2024-11-07 21:00:00 +0100

    config admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Dataset
 
-# Register your models here.
+
+class DatasetAdmin(admin.ModelAdmin):
+
+    list_display = ("filename", "download_date", "id")
+    search_fields = ("filename",)
+    list_filter = ("download_date",)
+    ordering = ("-download_date",)
+    list_per_page = 20
+
+
+admin.site.register(Dataset, DatasetAdmin)
